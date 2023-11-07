bc62f0cfc568d7c369461c28a1ff9495597ec1f1
sajalsardar <sajal2.stars@gmail.com>
2023-11-07 13:25:29 +0600

    apply job handle method

--- app/Jobs/ProcessUserInfoCsv.php
+++ app/Jobs/ProcessUserInfoCsv.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Models\UserInfo;
 use Illuminate\Bus\Batchable;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -12,20 +13,23 @@ use Illuminate\Queue\SerializesModels;
 class ProcessUserInfoCsv implements ShouldQueue {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, Batchable;
 
-    protected $userInfo;
+    protected $userInfos;
     protected $header;
     /**
      * Create a new job instance.
      */
     public function __construct($userInfo, $header) {
-        $this->header   = $header;
-        $this->userInfo = $userInfo;
+        $this->header    = $header;
+        $this->userInfos = $userInfo;
     }
 
     /**
      * Execute the job.
      */
     public function handle(): void {
-        //
+        foreach ($this->userInfos as $userInfo) {
+            $combineData = array_combine($this->header, $userInfo);
+            UserInfo::create($combineData);
+        }
     }
 }
